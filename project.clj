f4707e3c4fe92b6f6a9d6b96545b4ad6158fa5b9
Miller Peterson <miller.peterson@gmail.com>
2016-08-01 19:19:43 -0400

    Add marginalia plugin

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject scad-stimergy "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :description "Creating OpenSCAD models via discrete stigmergy algorithms."
+  :url "http://github.com/millerpeterson/scad-stigmergy"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]]
+  :plugins [[lein-marginalia "0.9.0"]]
   :main ^:skip-aot scad-stimergy.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
